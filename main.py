7b689a76d18495251b43f3481d1f2d11d15c28ee
CapitalKage <capitalkage>
2021-11-18 21:56:59 -0800

    refined the structure and flow of the game

--- main.py
+++ main.py
@@ -1,55 +1,49 @@
 # Rock, Paper, Scissors game with Python
 import random
 
-# variable for the NPC selection. Based on a random Integer between range of 0 - 2.
-npc_selection = random.randint(0, 2)
-# variable to store the computers score
-npc_score = 0
-# variable to store the players score
-player_score = 0
-player_selection = input("Please selection your choice [0] Rock, [1] Paper, and [2] Scissors. \n>")
-# variable for collection the players input based on integer selection/assignment.
-converted_selection = int(player_selection)
+
+def game_loop(game_ongoing=True, npc=0, player=0):
+    while game_ongoing:
+        player_choice = input('Please select [1] Rock, [2] Scissors, and [3] Paper')
+
+        if player_choice == '1':
+            print('Player selects ROCK')
+        elif player_choice == '2':
+            print('Player selects SCISSORS')
+        else:
+            print('Player selects PAPER')
+
+        computer_choice = random.randint(1, 3)
+
+        if computer_choice == 1:
+            print('NPC selects ROCK')
+        elif computer_choice == 2:
+            print('NPC selects SCISSORS')
+        else:
+            print('NPC selects PAPER')
+
+        if computer_choice == 1 and player_choice == '2':
+            npc += 1
+        elif computer_choice == 2 and player_choice == '3':
+            npc += 1
+        elif computer_choice == 3 and player_choice == '1':
+            player += 1
+        elif computer_choice == 1 and player_choice == '3':
+            npc += 1
+        elif computer_choice == 2 and player_choice == '1':
+            player += 1
+        elif computer_choice == 3 and player_choice == '2':
+            player += 1
+        else:
+            print('Game is a tie')
+
+        print('Player score {} NPC score {}'.format(player, npc))
+        if npc >= 3 and npc > player:
+            print('NPC is the winner! You suck!')
+            break
+        elif player >= 3 and player > npc:
+            print('Hey you finally won!')
+            break
 
 
-# Function for getting NPC choice.
-def npc_turn():
-    selection = npc_selection
-    if selection == 0:
-        return "NPC selects rock"
-    elif selection == 1:
-        return "NPC selects Paper"
-    else:
-        return "NPC selects Scissors"
-
-
-# Victory/scoring of game
-def scoring():
-    global player_score, npc_score
-    if converted_selection == 0 and npc_selection == 1:
-        npc_score += 1
-    elif converted_selection == 1 and npc_selection == 2:
-        npc_score += 1
-    elif converted_selection == 2 and npc_selection == 0:
-        npc_score += 1
-    elif converted_selection == 0 and npc_selection == 2:
-        player_score += 1
-    elif converted_selection == 1 and npc_selection == 0:
-        player_score += 1
-    elif converted_selection == 2 and npc_selection == 1:
-        player_score += 1
-    else:
-        return "Match is a tie"
-
-
-def round_one():
-    npc_turn()
-    print(npc_turn())
-    scoring()
-    print("Players score is ", player_score)
-    print("NPC score is ", npc_score)
-
-
-round_one()
-# Testing NPC_selection is working correctly and is the proper range.
-# print(npc_selection)
+game_loop(True, 0, 0)
